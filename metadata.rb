506a950d95d7d84c42e2166e597501a3e3c756a8
Ben Langfeld <ben@langfeld.me>
2013-11-13 10:23:49 -0200

    Clean up metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
-maintainer        "Mojo Lingo LLC"
-maintainer_email  "adam@mojolingo.com"
-license           "Proprietary"
-description       "Installs Adam Snark Rabbit"
+name              'adam_snark_rabbit'
+maintainer        'Mojo Lingo LLC'
+maintainer_email  'ops@mojolingo.com'
+license           'Proprietary'
+description       'Installs Adam Snark Rabbit'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
-recipe            "adam", "Installs ruby, checks out adam"
+version           '0.1.0'
+recipe            'adam_snark_rabbit', 'Installs all of Adam Snark Rabbit'
 
 %w{ ubuntu debian }.each do |os|
   supports os
