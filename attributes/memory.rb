ef5292ad296e4812f89710b1e34cd5e0d6516bb0
Ben Langfeld <ben@langfeld.me>
2013-11-26 18:26:31 -0200

    Default to localhost app server for nginx proxy

--- attributes/memory.rb
+++ attributes/memory.rb
@@ -3,4 +3,4 @@ default['adam']['memory']['internal_username'] = 'internal'
 default['adam']['memory']['internal_password'] = 'abc123'
 default['adam']['memory']['mongoid_host'] = '127.0.0.1'
 default['adam']['memory']['bosh_host'] = 'localhost'
-default['adam']['memory']['application_servers'] = []
+default['adam']['memory']['application_servers'] = ['127.0.0.1']
