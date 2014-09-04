d0cef5d957510f0f2929c2edec39e7a60788b7cd
Ben Langfeld <ben@langfeld.me>
2014-09-04 15:46:56 -0300

    Deploy the simple version by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default['adam']['environment']            = "production"
 default['adam']['standalone_deployment']  = true
 default['adam']['deployment_path']        = "/srv/adam"
 default['adam']['app_repo_url']           = "git@github.com:mojolingo/Adam.Snark.Rabbit.git"
-default['adam']['app_repo_ref']           = "master"
+default['adam']['app_repo_ref']           = "feature/simple"
 default['adam']['deploy_key']             = nil
 
 default['adam']['root_domain']      = node['fqdn']
